6f427295c4e050bdd626443306dafe17be6d1e7b
zoya111 <zoyairaqui90@gmail.com>
2018-10-31 17:51:57 +0530

    Update quiz1.py

--- quiz1.py
+++ quiz1.py
@@ -39,7 +39,7 @@ str()
 
 #Create a program that asks the user to enter their name and their age.
 #  Print out a message addressed to them that tells them the year that they will turn 100 years old.
-name=input('enter your name')
+name=input('enter your name')       #name input
 age=int(input("enter your age"))
 a=2018-age
 b=a+100
